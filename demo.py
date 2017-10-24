124b8af729a6c73645b3d3f937a59e77d52bd911
yhenon <yannhenon@gmail.com>
2017-10-24 16:14:25 -0400

    cleanup

--- demo.py
+++ demo.py
@@ -1,22 +1,17 @@
 import rcc
-import pdb
 import numpy as np
 from sklearn.metrics import adjusted_mutual_info_score
 
 X = []
 Y = []
 
-with open('pendigits.txt', 'r') as f:
-	for line in f:
-		line_split = line.strip().replace(' ','').split(',')
-		x = np.array([int(s) for s in line_split[:-1]])
-		y = int(line_split[-1])
-		X.append(x)
-		Y.append(y)
+pendata = np.genfromtxt('pendigits.txt', delimiter=',')
 
-X = np.array(X).astype(np.float32)
-Y = np.array(Y)
-clusterer = rcc.rcc_cluster(measure='cosine')
+X = pendata[:,:-1]
+Y = pendata[:,-1]
+
+clusterer = rcc.RccCluster(measure='cosine')
+
 P = clusterer.fit(X)
-P = clusterer.labels_
+
 print('AMI: {}'.format(adjusted_mutual_info_score(Y, P)))
--- rcc.py
+++ rcc.py
@@ -1,61 +1,109 @@
-import numpy as np
-from numpy.linalg import norm, solve, lstsq
-
-from scipy.spatial.distance import cdist, pdist
-from scipy.spatial import distance
-from scipy.sparse import csr_matrix, triu, find
-from scipy.sparse.csgraph import minimum_spanning_tree, connected_components
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import normalize, scale
-
-import time
-import scipy.sparse
 import math
 
-eps = 1e-5
+import numpy as np
+import scipy.sparse
+import scipy.sparse.linalg
 
-class rcc_cluster():
-    def __init__(self, k=10, verbose=True, preprocessing='none', measure='euclidean', clustering_threshold=1.):
-        self.clustering_threshold = clustering_threshold
-        self.preprocessing = preprocessing
+from scipy.sparse import csr_matrix, triu, find
+from scipy.sparse.csgraph import minimum_spanning_tree, connected_components
+from scipy.spatial import distance
+
+
+class RccCluster:
+    """
+    Computes a clustering following: Robust continuous clustering, (Shaha and Koltunb, 2017).
+    The interface is based on the sklearn.cluster module.
+
+    Parameters
+    ----------
+    k (int) number of neighbors for each sample in X
+    measure (string) distance metric, one of 'cosine' or 'euclidean'
+    clustering_threshold (float) threshold to assign points together in a cluster. Higher means fewer larger clusters
+    eps (float) numerical epsilon used for computation
+    verbose (boolean) verbosity
+    """
+
+    def __init__(self, k=10, measure='euclidean', clustering_threshold=1., eps=1e-5, verbose=True):
+
         self.k = k
-        self.verbose = verbose
-        self.labels_ = []
         self.measure = measure
+        self.clustering_threshold = clustering_threshold
+        self.eps = eps
+        self.verbose = verbose
 
-    def assignment(self, epsilon):
-        diff = np.sum((self.U[self.i,:] - self.U[self.j,:])**2, axis=1)
+        self.labels_ = None
+        self.U = None
+        self.i = None
+        self.j = None
+        self.n_samples = None
 
-        # computing connected components. 
+    def compute_assignment(self, epsilon):
+        """
+        Assigns points to clusters based on their representative. Two points are part of the same cluster if their
+        representative are close enough (their squared euclidean distance is < delta)
+        """
+        diff = np.sum((self.U[self.i, :] - self.U[self.j, :])**2, axis=1)
+
+        # computing connected components.
         is_conn = np.sqrt(diff) <= self.clustering_threshold*epsilon
 
-        G = scipy.sparse.coo_matrix((np.ones((2*np.sum(is_conn),)), (np.concatenate([self.i[is_conn], self.j[is_conn]], axis=0), np.concatenate([self.j[is_conn], self.i[is_conn]],axis=0))), shape=[self.nsamples,self.nsamples])
+        G = scipy.sparse.coo_matrix((np.ones((2*np.sum(is_conn),)),
+                                     (np.concatenate([self.i[is_conn], self.j[is_conn]], axis=0),
+                                      np.concatenate([self.j[is_conn], self.i[is_conn]], axis=0))),
+                                    shape=[self.n_samples, self.n_samples])
 
         num_components, labels = connected_components(G, directed=False)
+
         return labels, num_components
 
-    def geman_mcclure(self, data, mu):
+    @staticmethod
+    def geman_mcclure(data, mu):
+        """
+        Geman McClure function. See Bayesian image analysis. An application to single photon emission tomography (1985).
+
+        Parameters
+        ----------
+        data (array) 2d numpy array of data
+        mu (float) scale parameter
+        """
         return (mu / (mu + np.sum(data**2, axis=1)))**2
 
-    def computeObj(self, X, U, lpq, i, j, landa, mu, weights, iter_num):
+    def compute_obj(self, X, U, lpq, i, j, lambda_, mu, weights, iter_num):
+        """
+        Computes the value of the objective function.
+
+        Parameters
+        ----------
+        X (array) data points, 2d numpy array of shape (n_features, n_clusters)
+        U (int) representative points, 2d numpy array of shape (n_features, n_clusters)
+        lpq (array) penalty term on the connections
+        i (array) first slice of w, used for convenience
+        j (array) second slice of w, used for convenience
+        lambda_ (float) term balancing the contributions of the losses
+        mu (float) scale parameter
+        weights (array) weights of the connections
+        iter_num (int) current iteration, only used for printing to screen if verbose=True
+        """
 
         # computing the objective as in equation [2]
-        data = 0.5 * np.sum(np.sum((X-U)**2))
-        diff = np.sum((U[i, :]-U[j,:])**2, axis=1)
-        smooth = landa * 0.5 * (np.inner(lpq*weights, diff) + mu * np.inner(weights, (np.sqrt(lpq+eps)-1)**2))
-            
-        # final objective  
+        data = 0.5 * np.sum(np.sum((X - U)**2))
+        diff = np.sum((U[i, :] - U[j, :])**2, axis=1)
+        smooth = lambda_ * 0.5 * (np.inner(lpq*weights, diff) + mu *
+                                  np.inner(weights, (np.sqrt(lpq + self.eps)-1)**2))
+
+        # final objective
         obj = data + smooth
         if self.verbose:
             print(' {} | {} | {} | {}'.format(iter_num, data, smooth, obj))
-    
+
         return obj
 
-    def mkNN(self, X, k, measure='euclidean'):
+    @staticmethod
+    def m_knn(X, k, measure='euclidean'):
         """
         This code is taken from:
         https://bitbucket.org/sohilas/robust-continuous-clustering/src/
+        The original terms of the license apply.
         Construct mutual_kNN for large scale dataset
 
         If j is one of i's closest neighbors and i is also one of j's closest members,
@@ -63,172 +111,212 @@ class rcc_cluster():
 
         Parameters
         ----------
-        X : [n_samples, n_dim] array
-        k : int
-          number of neighbors for each sample in X
+        X (array) 2d array of data of shape (n_samples, n_dim)
+        k (int) number of neighbors for each sample in X
+        measure (string) distance metric, one of 'cosine' or 'euclidean'
         """
 
         samples = X.shape[0]
-        batchsize = 10000
+        batch_size = 10000
         b = np.arange(k+1)
         b = tuple(b[1:].ravel())
 
-        z = np.zeros((samples,k))
+        z = np.zeros((samples, k))
         weigh = np.zeros_like(z)
 
         # This loop speeds up the computation by operating in batches
         # This can be parallelized to further utilize CPU/GPU resource
 
-        for x in np.arange(0, samples, batchsize):
+        for x in np.arange(0, samples, batch_size):
             start = x
-            end = min(x+batchsize,samples)
+            end = min(x+batch_size, samples)
 
             w = distance.cdist(X[start:end], X, measure)
 
             y = np.argpartition(w, b, axis=1)
 
-            z[start:end,:] = y[:, 1:k + 1]
-            weigh[start:end,:] = np.reshape(w[tuple(np.repeat(np.arange(end-start), k)), tuple(y[:, 1:k+1].ravel())], (end-start, k))
-            del(w)
+            z[start:end, :] = y[:, 1:k + 1]
+            weigh[start:end, :] = np.reshape(w[tuple(np.repeat(np.arange(end-start), k)),
+                                               tuple(y[:, 1:k+1].ravel())], (end-start, k))
+            del w
 
         ind = np.repeat(np.arange(samples), k)
 
-        P = csr_matrix((np.ones((samples*k)), (ind.ravel(), z.ravel())), shape=(samples,samples))
-        Q = csr_matrix((weigh.ravel(), (ind.ravel(), z.ravel())), shape=(samples,samples))
+        P = csr_matrix((np.ones((samples*k)), (ind.ravel(), z.ravel())), shape=(samples, samples))
+        Q = csr_matrix((weigh.ravel(), (ind.ravel(), z.ravel())), shape=(samples, samples))
 
         Tcsr = minimum_spanning_tree(Q)
         P = P.minimum(P.transpose()) + Tcsr.maximum(Tcsr.transpose())
         P = triu(P, k=1)
 
         V = np.asarray(find(P)).T
-        return V[:,:2].astype(np.int32)
+        return V[:, :2].astype(np.int32)
 
-    def RCC(self, X, w, maxiter=100, disp=True, inner_iter=4):
+    def run_rcc(self, X, w, max_iter=100, inner_iter=4):
+        """
+        Main function for computing the clustering.
 
-        count = 0
+        Parameters
+        ----------
+        X (array) 2d array of data of shape (n_samples, n_dim).
+        w (array) weights for each edge, as computed by the mutual knn clustering.
+        max_iter (int) maximum number of iterations to run the algorithm.
+        inner_iter (int) number of inner iterations. 4 works well in most cases.
+        """
 
-        X = X.astype(np.float32) # features stacked as N x D (D is the dimension)
-        assert len(X.shape) == 2
+        X = X.astype(np.float32)  # features stacked as N x D (D is the dimension)
 
-        w = w.astype(np.int32) # list of edges represented by start and end nodes
+        w = w.astype(np.int32)  # list of edges represented by start and end nodes
         assert w.shape[1] == 2
 
+        # slice w for convenience
         i = w[:, 0]
         j = w[:, 1]
 
         # initialization
-        nsamples, nfeatures = X.shape
+        n_samples, n_features = X.shape
 
-        npairs = w.shape[0]
+        n_pairs = w.shape[0]
 
         # precomputing xi
         xi = np.linalg.norm(X, 2)
 
-        # setting weights as given in equation [S1]
-        R = scipy.sparse.coo_matrix((np.ones((i.shape[0]*2,)), 
-            (np.concatenate([i, j], axis=0), np.concatenate([j, i],axis=0))), shape=[nsamples, nsamples])
+        # set the weights as given in equation [S1] (supplementary information), making sure to exploit the data
+        # sparsity
+        R = scipy.sparse.coo_matrix((np.ones((i.shape[0]*2,)),
+                                     (np.concatenate([i, j], axis=0),
+                                      np.concatenate([j, i], axis=0))), shape=[n_samples, n_samples])
 
-        nconn = np.sum(R, axis=1)
-        nconn = np.asarray(nconn)
+        # number of connections
+        n_conn = np.sum(R, axis=1)
 
-        weights = np.mean(nconn) / np.sqrt(nconn[i]*nconn[j])
-        weights = weights[:, 0]
+        # make sure to convert back to a numpy array from a numpy matrix, since the output of the sum() operation on a
+        # sparse matrix is a numpy matrix
+        n_conn = np.asarray(n_conn)
 
-        # initializing U to X and lpq = 1 forall p,q in E
+        # equation [S1]
+        weights = np.mean(n_conn) / np.sqrt(n_conn[i]*n_conn[j])
+        weights = weights[:, 0]  # squueze out the unnecessary dimension
+
+        # initializing the representatives U to have the same value as X
         U = X.copy()
+
+        # initialize lpq to 1, that is all connections are active
+        # lpq is a penalty term on the connections
         lpq = np.ones((i.shape[0],))
 
-        # computation of delta and mu
-        epsilon = np.sqrt(np.sum((X[i, :] - X[j, :])**2 + eps, axis=1))
+        # compute delta and mu, see SI for details
+        epsilon = np.sqrt(np.sum((X[i, :] - X[j, :])**2 + self.eps, axis=1))
 
-        # Note: suppress low values, but hard coded threshold has issues
-        epsilon[epsilon/np.sqrt(nfeatures) < 1e-2] = np.max(epsilon)
+        # Note: suppress low values. This hard coded threshold could lead to issues with very poorly normalized data.
+        epsilon[epsilon/np.sqrt(n_features) < 1e-2] = np.max(epsilon)
 
         epsilon = np.sort(epsilon)
 
+        # compute mu, see section Graduated Nonconvexity in the SI
         mu = 3.0 * epsilon[-1]**2
 
-        # top 1% of the closest neighbours
-        top_samples = np.minimum(250.0, math.ceil(npairs*0.01)) 
+        # take the top 1% of the closest neighbours as a heuristic
+        top_samples = np.minimum(250.0, math.ceil(n_pairs*0.01))
 
         delta = np.mean(epsilon[:int(top_samples)])
-        epsilon = np.mean(epsilon[:int(math.ceil(npairs*0.01))])
+        epsilon = np.mean(epsilon[:int(math.ceil(n_pairs*0.01))])
 
-        # computation of matrix A = D-R (here D is the diagonal matrix and R is the symmetric matrix)
-        R = scipy.sparse.coo_matrix((np.concatenate([weights*lpq, weights*lpq], axis=0), (np.concatenate([i,j],axis=0), np.concatenate([j,i],axis=0))), shape=[nsamples, nsamples])
-        D = scipy.sparse.coo_matrix((np.squeeze(np.asarray(np.sum(R,axis=1))), ((range(nsamples), range(nsamples)))), (nsamples, nsamples))
+        # computation of matrix A = D-R (here D is the diagonal matrix and R is the symmetric matrix), see equation (8)
+
+        R = scipy.sparse.coo_matrix((np.concatenate([weights*lpq, weights*lpq], axis=0),
+                                     (np.concatenate([i,j],axis=0), np.concatenate([j,i],axis=0))),
+                                    shape=[n_samples, n_samples])
+
+        D = scipy.sparse.coo_matrix((np.squeeze(np.asarray(np.sum(R,axis=1))),
+                                     ((range(n_samples), range(n_samples)))),
+                                    (n_samples, n_samples))
 
         # initial computation of lambda (lambda is a reserved keyword in python)
-        # note: the largest magnitude eigenvalue is equal to the L2 norm under certain conditions, and is faster to compute
+        # note: compute the largest magnitude eigenvalue instead of the matrix norm as it is faster to compute
 
         eigval = scipy.sparse.linalg.eigs(D-R, k=1, return_eigenvectors=False).real
-        landa =  xi / eigval[0]
+
+        # lambda is a reserved keyword in python, so we use lambda_. Calculate lambda as per equation 9.
+        lambda_ = xi / eigval[0]
 
         if self.verbose:
-            print('mu = {}, lambda = {}, epsilon = {}, delta = {}'.format(mu, landa, epsilon, delta))
+            print('mu = {}, lambda = {}, epsilon = {}, delta = {}'.format(mu, lambda_, epsilon, delta))
             print(' Iter | Data \t | Smooth \t | Obj \t')    
 
-        obj = np.zeros((maxiter,))
+        # pre-allocate memory for the values of the objective function
+        obj = np.zeros((max_iter,))
+
+        inner_iter_count = 0
 
         # start of optimization phase
-        starttime = time.time()
 
-        for iter_num in range(1, maxiter):
-            st = time.time()
-            # update lpq
-            lpq = self.geman_mcclure(U[i,:]-U[j,:], mu)
+        for iter_num in range(1, max_iter):
 
-            # compute objective
-            obj[iter_num] = self.computeObj(X, U, lpq, i, j, landa, mu, weights, iter_num)
+            # update lpq. Equation 5.
+            lpq = self.geman_mcclure(U[i, :]-U[j, :], mu)
+
+            # compute objective. Equation 6.
+            obj[iter_num] = self.compute_obj(X, U, lpq, i, j, lambda_, mu, weights, iter_num)
             
-            # update U's
+            # update U. Equation 7. For efficiency we form sparse matrices R and D separately, then combine them.
 
-            R = scipy.sparse.coo_matrix((np.concatenate([weights*lpq, weights*lpq], axis=0), (np.concatenate([i,j],axis=0), np.concatenate([j,i],axis=0))), shape=[nsamples, nsamples])
+            R = scipy.sparse.coo_matrix((np.concatenate([weights*lpq, weights*lpq], axis=0),
+                                         (np.concatenate([i,j],axis=0), np.concatenate([j,i],axis=0))),
+                                        shape=[n_samples, n_samples])
 
-            D = scipy.sparse.coo_matrix((np.asarray(np.sum(R, axis=1))[:, 0], ((range(nsamples), range(nsamples)))), shape=(nsamples, nsamples))
+            D = scipy.sparse.coo_matrix((np.asarray(np.sum(R, axis=1))[:, 0], ((range(n_samples), range(n_samples)))),
+                                        shape=(n_samples, n_samples))
 
-            M = scipy.sparse.eye(nsamples) + landa * (D-R)
-            U = scipy.sparse.linalg.spsolve(M,X)
+            M = scipy.sparse.eye(n_samples) + lambda_ * (D-R)
+
+            # Solve for U. This could be further optimised through appropriate preconditioning.
+            U = scipy.sparse.linalg.spsolve(M, X)
           
             # check for stopping criteria
-            count += 1
+            inner_iter_count += 1
 
-            if (abs(obj[iter_num-1]-obj[iter_num]) < 1e-1) or count == inner_iter:
+            # check for the termination conditions and modulate delta if necessary.
+            if (abs(obj[iter_num-1]-obj[iter_num]) < 1e-1) or inner_iter_count == inner_iter:
                 if mu >= delta:
                     mu /= 2.0
-                elif count == inner_iter:
+                elif inner_iter_count == inner_iter:
                     mu = 0.5 * delta
                 else:
                     break
 
                 eigval = scipy.sparse.linalg.eigs(D-R, k=1, return_eigenvectors=False).real
-                landa =  xi / eigval[0]
-                count = 0
+                lambda_ = xi / eigval[0]
+                inner_iter_count = 0
 
+        # at the end of the run, assign values to the class members.
         self.U = U.copy()
         self.i = i
         self.j = j
-        self.nsamples = nsamples
-        C, num_components = self.assignment(epsilon)
+        self.n_samples = n_samples
+        C, num_components = self.compute_assignment(epsilon)
 
         return U, C
 
-
     def fit(self, X):
+        """
+        Computes the clustering and returns the labels
+        Parameters
+        ----------
+        X (array) numpy array of data to cluster with shape (n_samples, n_features)
+        """
 
-        n_samples, n_features = X.shape
+        assert type(X) == np.ndarray
+        assert len(X.shape) == 2
 
-        if self.preprocessing == 'scale':
-            X = scale(X, copy=False)
-        elif self.preprocessing == 'minmax':
-            minmax_scale = MinMaxScaler().fit(X)
-            X = minmax_scale.transform(X)
-        elif self.preprocessing == 'normalization':
-            X = np.sqrt(n_features) * normalize(X, copy=False)
+        # compute the mutual knn graph
+        mknn_matrix = self.m_knn(X, self.k, measure=self.measure)
 
-        mknn_matrix = self.mkNN(X, self.k, measure=self.measure)
-        U, C = self.RCC(X, mknn_matrix)
+        # perform the RCC clustering
+        U, C = self.run_rcc(X, mknn_matrix)
+
+        # store the class labels in the appropriate class member to match the sklearn.cluster interface
         self.labels_ = C.copy()
 
+        # return the computed labels
         return self.labels_
-
